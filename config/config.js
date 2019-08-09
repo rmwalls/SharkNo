d4a7b645f82d1b13326f7ff80fce7527dc51f307
dziubliukbm <bohdan.dz.92@gmail.com>
2019-08-09 17:53:34 -0400

    add styles

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": process.env.mysqlPassword,
-    "database": "exampledb",
+    "database": "example_db",
     "host": "localhost",
     "dialect": "mysql"
   },
